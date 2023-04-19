f80d91d5793b43a1dc6ee0c56dcc4764f679cb70
unknown <nguyenducbaokey@gmail.com>
2023-04-19 15:40:44 +0700

    Profiles configuration and Deploy

--- src/main/java/com/example/Springboot/demo/controller/HelloController.java
+++ src/main/java/com/example/Springboot/demo/controller/HelloController.java
@@ -1,5 +1,6 @@
 package com.example.Springboot.demo.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -7,10 +8,12 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
+    @Value("${welcome.message}")
+    private String welcomeMessage;
 
     //    @RequestMapping(value = "/",method = RequestMethod.GET)
     @GetMapping("/")
     public String helloWorld() {
-        return "Welcome to Daily Code Buffer sadsadasdasd!!";
+        return welcomeMessage;
     }
 }
